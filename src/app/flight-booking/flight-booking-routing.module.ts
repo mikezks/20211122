579b73c84f6d6bf8d5f93619726e1105945b4d2f
michael.zikes <michael.zikes@intauria.com>
2021-12-14 14:07:54 +0100

    Routing: feature module

---
+++ src/app/flight-booking/flight-booking-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FlightEditComponent } from './feature/flight-edit/flight-edit.component';
+import { FlightSearchComponent } from './feature/flight-search/flight-search.component';
+
+const routes: Routes = [
+  {
+    path: 'flight-booking',
+    children: [
+      {
+        path: 'flight-search',
+        component: FlightSearchComponent
+      },
+      {
+        path: 'flight-edit',
+        component: FlightEditComponent
+      }
+    ]
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FlightBookingRoutingModule { }
--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -3,19 +3,20 @@ import { CommonModule } from '@angular/common';
 import { FlightSearchComponent } from './feature/flight-search/flight-search.component';
 import { SharedModule } from '../shared/shared.module';
 import { FlightCardComponent } from './ui/flight-card/flight-card.component';
+import { FlightBookingRoutingModule } from './flight-booking-routing.module';
+import { FlightEditComponent } from './feature/flight-edit/flight-edit.component';
 
 
 @NgModule({
   declarations: [
     FlightSearchComponent,
-    FlightCardComponent
+    FlightCardComponent,
+    FlightEditComponent
   ],
   imports: [
     CommonModule,
+    FlightBookingRoutingModule,
     SharedModule
-  ],
-  exports: [
-    FlightSearchComponent
   ]
 })
 export class FlightBookingModule { }
